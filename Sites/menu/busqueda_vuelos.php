076a4270d5e910acfe3fec6408c4bd5b8b2c4e35
ignacio-quera <ignacio.quera@uc.cl>
2022-06-26 19:55:42 -0400

    procedimiento almacenado

--- Sites/menu/busqueda_vuelos.php
+++ Sites/menu/busqueda_vuelos.php
@@ -45,6 +45,10 @@
         $destino = $_GET['destino'];
         $fecha = $_GET['fecha'];
 
+        $_SESSION['origen'] = $origen;
+        $_SESSION['destino'] = $destino;
+        $_SESSION['fecha'] = $fecha;
+
         $query_vuelos = "SELECT companiaaerea.nombre, vuelos.codigovuelo, origenes.ciudad, vuelos.fechasalida, destinos.ciudad, vuelos.fechallegada
                          FROM vuelos, companiaaerea, (SELECT aerodromo.codigoicao as orig, aerodromo.ciudad as ciudad
                                                       FROM aerodromo
@@ -127,6 +131,9 @@
                                 <td>$datos[5]</td>
                                 <td>
                                     <form method='GET' action='reservar.php'>
+                                        <input type='hidden' name='origen' value='$origen' />
+                                        <input type='hidden' name='destino' value='$destino' />
+                                        <input type='hidden' name='fecha' value='$fecha' />
                                         <input type='submit' value='Reservar'/>
                                     </form>
                                 </td>
--- Sites/menu/reservar.php
+++ Sites/menu/reservar.php
@@ -1,13 +1,42 @@
 <?php
     session_start();
     $pasaporte = $_SESSION['user'];
+    $origen = $_SESSION['origen'];
+    $destino = $_SESSION['destino'];
+    $fecha = $_SESSION['fecha'];
     if (is_null($pasaporte)) {
         header("Location: https://codd.ing.puc.cl/~grupo57/index.php?");
         exit();
     }
     include('../templates/header.html');
-?>
 
+    require("../config/conexion.php");
+
+    if (!isset($_GET['pasaporte1']) || !isset($_GET['pasaporte2']) || !isset($_GET['pasaporte3'])) {
+        $_GET['pasaporte1'] = '';
+        $_GET['pasaporte2'] = '';
+        $_GET['pasaporte3'] = '';
+        $mensaje = null;    
+    }    
+    else  {
+        $pasaporte1 = $_GET['pasaporte1'];
+        $pasaporte2 = $_GET['pasaporte2'];
+        $pasaporte3 = $_GET['pasaporte3'];
+
+
+        echo "$origen, $destino, $fecha, $pasaporte, $pasaporte1, $pasaporte2, $pasaporte3";
+
+        $query = "SELECT * from generar_reserva('$origen', '$destino', '$fecha', '$pasaporte',
+                            '$pasaporte1', '$pasaporte2', '$pasaporte3');";
+        $db1 ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $result = $db1 -> prepare($query);
+        $result -> execute();
+        $data = $result -> fetchAll();
+        
+        print_r($data);
+        $mensaje = $data[0];
+    }
+?>  
 <h1 align="center">Ingrese Los Pasaportes Para</h1>
 <h1 align="center">Quienes Desee Hacer La Reserva</h1>
 <br>
@@ -15,22 +44,19 @@
 <div style="margin-right:50px; margin-left:50px;" >
 
 <!-- action="/send.php"  poner lo anterior para mandar la info a otra página -->
-    <form align="center" name="reserva1" method="post">
-    <input type="text" name="pasaporte1" autocomplete="off"
+    <form align="center" name="reserva" method="get">
+    <input type="text" name="pasaporte1" id="pasaporte1" autocomplete="off"
             placeholder="Introducir Pasaporte" />
-    </form>
     <br>
-    <form align="center" name="reserva2" method="post">
-    <input type="text" name="pasaporte2" autocomplete="off"
+    <br>
+    <input type="text" name="pasaporte2" id="pasaporte2" autocomplete="off"
             placeholder="Introducir Pasaporte" />
-    </form>
     <br>
-    <form align="center" name="reserva3" method="post">
-    <input type="text" name="pasaporte3" autocomplete="off"
+    <br>
+    <input type="text" name="pasaporte3" id="pasaporte3" autocomplete="off"
             placeholder="Introducir Pasaporte" />
-    </form>
     <br>
-    <form align="center" method="get">
+    <br>
     <input type="submit" value="Generar Reserva">
     </form>
 
@@ -39,5 +65,47 @@
 <form align="center" action="busqueda_vuelos.php" method="get">
     <input type="submit" value="Volver">
 </form>
+<?php
+                if (!is_null($mensaje)) {
+                    if ($mensaje = 'No se ingresaron pasajeros para reservar') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("Se debe ingresar por lo menos un pasajero!");
+                        </script>';
+                    }
+                    elseif ($mensaje ='No existen lo pasajaeros con el primer pasaporte') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El primer pasaporte no existe!");
+                        </script>';
+                    } elseif ($mensaje ='No existen lo pasajaeros con el segundo pasaporte') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El segundo pasaporte no existe!");
+                        </script>';
+                    } elseif ($mensaje ='No existen lo pasajaeros con el tercer pasaporte') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El tercer pasaporte no existe!");
+                        </script>';
+                    } elseif ($mensaje ='Choque temporal del pasajero de pasaporte $pasaporte1') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El pasajero de pasaporte $pasaporte1 tiene un tope con otro vuelo!");
+                        </script>';
+                    } elseif ($mensaje ='Choque temporal del pasajero de pasaporte $pasaporte2') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El pasajero de pasaporte $pasaporte2 tiene un tope con otro vuelo!");
+                        </script>';
+                    } elseif ($mensaje ='Choque temporal del pasajero de pasaporte $pasaporte3') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("El pasajero de pasaporte $pasaporte3 tiene un tope con otro vuelo!");
+                        </script>';
+                    } elseif ($mensaje ='reserva creada') {
+                        echo '<script type="text/JavaScript"> 
+                        prompt("La reserva fue creada con éxito!");
+                        </script>';
+                    } 
+                }
+                    
+            ?>
+
+<br>
+<br>
 </body>
 </html>
