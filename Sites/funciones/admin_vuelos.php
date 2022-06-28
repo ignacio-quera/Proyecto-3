13ba57b6989988216f711d06df209070ce252aa2
Francisco Rodriguez <farodriguezhu@uc.cl>
2022-06-28 16:26:56 -0400

    Aceptar/rechazar listo

--- Sites/funciones/admin_vuelos.php
+++ Sites/funciones/admin_vuelos.php
@@ -14,40 +14,44 @@
               AND fpl.propuesta_vuelo_id = propuestavuelo.propuesta_vuelo_id;";
     $result = $db2 -> prepare($query);
     $result -> execute();
-    $data = $result -> fetchAll();
+    $data = $result -> fetch();
 
     // Buscar id
     $queryid = "SELECT MAX(vuelos.idvuelo)
               FROM vuelos;";
     $resultid = $db1 -> prepare($queryid);
     $resultid -> execute();
-    $id_max = $resultid -> fetchAll();
+    $id_max = $resultid -> fetch();
+    $id_max = $id_max[0] + 1;
 
     if($_POST['estado'] == 'aceptar'){
         $query1 = "UPDATE fpl
                    SET estado = 'aceptado'
                    WHERE fpl.propuesta_vuelo_id = '$id_vuelo';";
-        //$result1 = $db2 -> prepare($query1);
-        //$result1 -> execute();
+        $result1 = $db2 -> prepare($query1);
+        $result1 -> execute();
 
-        $query2 = "INSERT INTO TABLE vuelos VALUES(
-                   '$data[0][0]', '$data[0][1]', '$data[0][2]', '$data[0][3]', '$data[0][4]',
-                   '$data[0][5]', '$data[0][6]', '$data[0][7]', '$data[0][8]', '$data[0][9]',
+        $query2 = "INSERT INTO vuelos VALUES( '$id_max',
+                   '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]',
+                   '$data[5]', '$data[6]', '$data[7]', '$data[8]', '$data[9]',
                    'aceptado');";
+        $result2 = $db1 -> prepare($query2);
+        $result2 -> execute();
     } elseif ($_POST['estado'] == 'rechazar') {
         $query1 = "UPDATE fpl
                    SET estado = 'rechazado'
                    WHERE fpl.propuesta_vuelo_id = '$id_vuelo';";
-        //$result1 = $db2 -> prepare($query1);
-        //$result1 -> execute();
+        $result1 = $db2 -> prepare($query1);
+        $result1 -> execute();
 
-        $query2 = "INSERT INTO TABLE vuelos VALUES(
-                   '$data[0][0]', '$data[0][1]', '$data[0][2]', '$data[0][3]', '$data[0][4]',
-                   '$data[0][5]', '$data[0][6]', '$data[0][7]', '$data[0][8]', '$data[0][9]',
+        $query2 = "INSERT INTO vuelos VALUES( '$id_max',
+                   '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]',
+                   '$data[5]', '$data[6]', '$data[7]', '$data[8]', '$data[9]',
                    'rechazado');";
+        $result2 = $db1 -> prepare($query2);
+        $result2 -> execute();
     }
 
-    //header("Location: ../menu/admin.php");
-    //exit();
-    echo $id_max[0][0] + 1;
+    header("Location: ../menu/admin.php");
+    exit();
 ?>
